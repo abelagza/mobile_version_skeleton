851f5778544b92c17f4d783238460dfdbfa200e9
abelagza <jabelagza@gmail.com>
2022-04-14 01:03:40 -0500

    Fix burger button color

--- features.js
+++ features.js
@@ -1,12 +1,25 @@
 const hamburger = document.getElementById("hamburger");
 const navUl = document.getElementById("nav-ul");
+const blur = document.getElementById("body");
+const blur1 = document.getElementById("projects");
+const blur2 = document.getElementById("about");
+const blur3 = document.getElementById("contact");
+
 
 hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show');
     hamburger.classList.toggle('show');
+    blur.classList.toggle('show');
+    blur1.classList.toggle('show');
+    blur2.classList.toggle('show');
+    blur3.classList.toggle('show');
 })
 
 document.querySelectorAll(".h-bttn").forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove("show");
     navUl.classList.remove("show"); 
+    blur.classList.remove("show");
+    blur1.classList.toggle('show');
+    blur2.classList.toggle('show');
+    blur3.classList.toggle('show');
 }))
