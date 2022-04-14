1f5d287b8bd7a3337d3e5a13cf1745b86c0fa2bd
abelagza <jabelagza@gmail.com>
2022-04-14 00:26:24 -0500

    fix burger icon styling

--- features.js
+++ features.js
@@ -3,10 +3,10 @@ const navUl = document.getElementById("nav-ul");
 
 hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show');
-    hamburger.classList.toggle('show')
+    hamburger.classList.toggle('show');
 })
 
 document.querySelectorAll(".h-bttn").forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove("show");
-    navUl.classList.remove("show");
+    navUl.classList.remove("show"); 
 }))
