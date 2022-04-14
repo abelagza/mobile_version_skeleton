4956bd171b3e73ea9c6eff65ca8f30d17fd155c0
abelagza <jabelagza@gmail.com>
2022-04-14 01:24:15 -0500

    Fix blured elements

--- features.js
+++ features.js
@@ -4,6 +4,7 @@ const blur = document.getElementById("body");
 const blur1 = document.getElementById("projects");
 const blur2 = document.getElementById("about");
 const blur3 = document.getElementById("contact");
+const aLogo = document.getElementById("logo");
 
 
 hamburger.addEventListener('click', () => {
@@ -13,13 +14,15 @@ hamburger.addEventListener('click', () => {
     blur1.classList.toggle('show');
     blur2.classList.toggle('show');
     blur3.classList.toggle('show');
+    aLogo.classList.toggle('show');
 })
 
 document.querySelectorAll(".h-bttn").forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove("show");
     navUl.classList.remove("show"); 
     blur.classList.remove("show");
-    blur1.classList.toggle('show');
-    blur2.classList.toggle('show');
-    blur3.classList.toggle('show');
+    blur1.classList.remove('show');
+    blur2.classList.remove('show');
+    blur3.classList.remove('show');
+    aLogo.classList.remove('show');
 }))
